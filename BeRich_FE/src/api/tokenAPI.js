7e1063d9a37a172b4c2d2da556c2628eafbff9a2
Minje Sung <minje813@dgu.ac.kr>
2024-08-05 13:43:40 +0900

    FE: [feat] Access Token 재발급 API 연동

---
+++ BeRich_FE/src/api/tokenAPI.js
@@ -0,0 +1,47 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { getAccessToken } from "./authAPI";
+
+// Axios 인스턴스 생성
+export const fetchAPI = axios.create()
+
+fetchAPI.interceptors.request.use(
+    async (config) => {
+        const accessTokenforAPI = await AsyncStorage.getItem('user_access_token');
+
+        // AccessToken이 있다면 헤더에 토큰 추가
+        if (accessTokenforAPI) {
+            config.headers = config.headers || {};
+            config.headers.Authorization = `Bearer ${accessTokenforAPI}`;
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+fetchAPI.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config
+
+        if (error.response) {
+            const errorStatus = error.response.data.status
+            const errorMessage = error.response.data.message
+
+            switch (errorStatus) {
+                case 403:
+                    console.error('Access Token 만료, '+errorMessage)
+                    await getAccessToken() // AccessToken 새로 불러오기
+                    const newAccessToken = await AsyncStorage.getItem('user_access_token');
+                    console.log('새로운 Access Token : '+ newAccessToken)
+
+                    // header 설정 후 재실행
+                    originalRequest.headers.Authorization = `Bearer ${newAccessToken}`
+                    return fetchAPI(originalRequest)
+            }
+        }
+        return Promise.reject(error);
+    }
+)

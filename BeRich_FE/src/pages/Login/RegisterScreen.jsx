b6b2b71c210f7cc02f60d2ad835baacaebd4a33d
Minje Sung <minje813@dgu.ac.kr>
2024-08-18 02:12:47 +0900

    FE: [fix] 로그인/회원가입 시 입력하는 창에 맞춰 스크롤이 옮겨짐

--- BeRich_FE/src/pages/Login/RegisterScreen.jsx
+++ BeRich_FE/src/pages/Login/RegisterScreen.jsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { KeyboardAvoidingView, ScrollView, View } from 'react-native';
 import { Button } from '@rneui/base';
 import { BoxStyles } from '../../styles/Box.style';
 import { ButtonStyles } from '../../styles/Button.style';
@@ -19,30 +19,34 @@ export default function RegisterScreen({ navigation }) {
     const [date, setDate] = useState(new Date())
 
     return (
-        <View style={BoxStyles.P10}>
-            <View style={[BoxStyles.MainBox, BoxStyles.P10]}>
-                <LabelInput label={'ID'} placeholder={'아이디를 입력해주세요'} state={id} setState={setId}></LabelInput>
-                <LabelSecretInput label={'PW'} placeholder={'비밀번호를 입력해주세요'} state={password} setState={setPassword}></LabelSecretInput>
-                <EmailInput label={'E-mail'} placeholder={'이메일 ID'}
-                    emailId={emailId} setEmailId={setEmailId} selectedDomain={selectedDomain} setSelectedDomain={setSelectedDomain}></EmailInput>
-                <NameInput label={'이름'} placeholder1={'성'} placeholder2={'이름'}
-                    state1={fName} setState1={setFName} state2={sName} setState2={setSName}></NameInput>
-                <DateInput label={'생년월일'} date={date} setDate={setDate}></DateInput>
-                <Button buttonStyle={ButtonStyles.MainButton} title={'회원가입'} titleStyle={[TextStyles.FwBold]}
-                    onPress={async () => {
-                        if (!CheckSignUp(id, password, emailId, selectedDomain, fName, sName, date)) return // 입력했는지 체크
-                        // id, email 중복체크
-                        const checkDuplicate = await CheckDuplicate(id, `${emailId}@${selectedDomain}`)
-                        if (!checkDuplicate) return
-                        // 회원가입
-                        handleRegister(id, password, `${emailId}@${selectedDomain}`, fName, sName, dateFormat(date), navigation)
-                    }}></Button>
-                <Button buttonStyle={ButtonStyles.InputButton} titleStyle={TextStyles.Detail} title={'서비스를 이용해 본 적 있는거 같아요!'}
-                    onPress={() => {
-                        navigation.replace('Login')
-                    }} />
-            </View>
-        </View>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === "ios" ? 'padding' : 'height'}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                <View style={BoxStyles.P10}>
+                    <View style={[BoxStyles.MainBox, BoxStyles.P10]}>
+                        <LabelInput label={'ID'} placeholder={'아이디를 입력해주세요'} state={id} setState={setId}></LabelInput>
+                        <LabelSecretInput label={'PW'} placeholder={'비밀번호를 입력해주세요'} state={password} setState={setPassword}></LabelSecretInput>
+                        <EmailInput label={'E-mail'} placeholder={'이메일 ID'}
+                            emailId={emailId} setEmailId={setEmailId} selectedDomain={selectedDomain} setSelectedDomain={setSelectedDomain}></EmailInput>
+                        <NameInput label={'이름'} placeholder1={'성'} placeholder2={'이름'}
+                            state1={fName} setState1={setFName} state2={sName} setState2={setSName}></NameInput>
+                        <DateInput label={'생년월일'} date={date} setDate={setDate}></DateInput>
+                        <Button buttonStyle={ButtonStyles.MainButton} title={'회원가입'} titleStyle={[TextStyles.FwBold]}
+                            onPress={async () => {
+                                if (!CheckSignUp(id, password, emailId, selectedDomain, fName, sName, date)) return // 입력했는지 체크
+                                // id, email 중복체크
+                                const checkDuplicate = await CheckDuplicate(id, `${emailId}@${selectedDomain}`)
+                                if (!checkDuplicate) return
+                                // 회원가입
+                                handleRegister(id, password, `${emailId}@${selectedDomain}`, fName, sName, dateFormat(date), navigation)
+                            }}></Button>
+                        <Button buttonStyle={ButtonStyles.InputButton} titleStyle={TextStyles.Detail} title={'서비스를 이용해 본 적 있는거 같아요!'}
+                            onPress={() => {
+                                navigation.replace('Login')
+                            }} />
+                    </View>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
 

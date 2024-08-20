56ff9954a9abfae4ad739c97e7ab3ef0ef8ddb2d
Minje Sung <minje813@dgu.ac.kr>
2024-08-20 18:33:50 +0900

    FE: [fix] 회원가입 이후 바로 자동거래 시도할 때 계좌정보를 입력하지 않아도 되는 오류 수정

--- BeRich_FE/src/pages/AfterLogin/AutoTradeScreen.jsx
+++ BeRich_FE/src/pages/AfterLogin/AutoTradeScreen.jsx
@@ -13,6 +13,7 @@ import { StartTradeComponent, StopTradeComponent } from '../../components/TradeC
 import { StartTradeModal } from '../../components/Modals';
 import { getTradeInfoAPI } from '../../api/getTradeInfoAPI';
 import { useFocusEffect } from '@react-navigation/native';
+import { getUserInfoAPI } from '../../api/getUserInfoAPI';
 
 export default function AutoTradeScreen({ navigation }) {
     const { state, setState } = useContext(AppContext);
@@ -43,6 +44,14 @@ export default function AutoTradeScreen({ navigation }) {
         setState((prevContext) => ({
             ...prevContext,
             statusData: data,
+        }))
+    }
+
+    // 계좌 상태 반영
+    const setIsAccount = (isAccount) => {
+        setState((prevContext) => ({
+            ...prevContext,
+            isAccount: isAccount,
         }))
     }
 
@@ -84,6 +93,15 @@ export default function AutoTradeScreen({ navigation }) {
                 setStatus([{ "endDay": "", "id": 0, "investmentInsight": "", "investmentPropensity": "", "startBalance": "", "startDay": "", "status": "ENDED", "stockCode": "", "totalProfit": "" }])
             }
         }
+        async function getBalanceData() {
+            const userInfoData = await getUserInfoAPI()
+            if (userInfoData.accountNum == "등록된 계좌 없음") {
+                setIsAccount(false)
+                return
+            }
+            setIsAccount(true)
+        }
+        getBalanceData();
         getTradeInfoData();
     }, []))
 
